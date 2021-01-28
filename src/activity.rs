24acc6679befd037cda4de6e85bfdc031aedf7c7
MDeiml <matthias@deiml.net>
2021-01-28 15:52:25 +0100

    Cache activity stats

--- src/activity.rs
+++ src/activity.rs
@@ -12,7 +12,7 @@ use sled::Transactional;
 const ACTIVITIES_TREE: &[u8] = b"activities";
 const ACTIVITIES_USER_TREE: &[u8] = b"activities_user";
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Activity {
     group_id: u64,
     start: u64,
@@ -20,9 +20,13 @@ pub struct Activity {
     description: String,
     min_participants: u32,
     max_participants: u32,
+    #[serde(default)]
+    accepted: u32,
+    #[serde(default)]
+    pending: u32,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, PartialEq, Eq)]
 pub enum Status {
     Pending,
     Accepted,
@@ -51,12 +55,9 @@ pub async fn create(
                 sled::transaction::abort(Abort::NotFound)?;
             }
             let activity_id = activities_tree.generate_id()?;
-            activities_tree.insert(
-                &activity_id.to_be_bytes(),
-                bincode::serialize(&activity).map_err(|err| {
-                    sled::transaction::ConflictableTransactionError::Abort(Abort::BincodeError(err))
-                })?,
-            )?;
+
+            let mut activity = activity.clone();
+            activity.pending = group.users.len() as u32;
             let mut key = Vec::with_capacity(16);
             for (user_id, _) in group.users {
                 key.clear();
@@ -72,6 +73,13 @@ pub async fn create(
                     })?,
                 )?;
             }
+
+            activities_tree.insert(
+                &activity_id.to_be_bytes(),
+                bincode::serialize(&activity).map_err(|err| {
+                    sled::transaction::ConflictableTransactionError::Abort(Abort::BincodeError(err))
+                })?,
+            )?;
             Ok(activity_id)
         },
     );
@@ -90,41 +98,6 @@ pub async fn create(
 pub struct ActivityStats {
     activity: Activity,
     status: Status,
-    accepted: u32,
-    pending: u32,
-}
-
-fn count_status(db: &sled::Db, activity_id: u64) -> Result<(u32, u32, Activity), Error> {
-    let activities_tree = db.open_tree(ACTIVITIES_TREE)?;
-    let activities_user_tree = db.open_tree(ACTIVITIES_USER_TREE)?;
-    let groups_tree = db.open_tree(crate::group::GROUPS_TREE)?;
-
-    let activity = activities_tree
-        .get(activity_id.to_be_bytes())?
-        .expect("Missing activity_id");
-    let activity: Activity = bincode::deserialize(&activity)?;
-    let group = groups_tree
-        .get(activity.group_id.to_be_bytes())?
-        .expect("Missing group_id");
-    let group: Group = bincode::deserialize(&group)?;
-    let mut pending = 0;
-    let mut accepted = 0;
-    let mut key = Vec::with_capacity(16);
-    for (user_id, _) in group.users {
-        key.clear();
-        key.extend_from_slice(&user_id.to_be_bytes());
-        key.extend_from_slice(&activity_id.to_be_bytes());
-        let user_status = activities_user_tree
-            .get(&key)?
-            .expect("Missing status for activity");
-        let user_status = bincode::deserialize(&user_status)?;
-        match user_status {
-            Status::Pending => pending += 1,
-            Status::Accepted => accepted += 1,
-            _ => (),
-        }
-    }
-    Ok((pending, accepted, activity))
 }
 
 pub async fn list(
@@ -132,6 +105,7 @@ pub async fn list(
     db: web::Data<sled::Db>,
 ) -> Result<HttpResponse, Error> {
     let user_id: u64 = session.get(&db)?;
+    let activities_tree = db.open_tree(ACTIVITIES_TREE)?;
     let activities_user_tree = db.open_tree(ACTIVITIES_USER_TREE)?;
     let activities = activities_user_tree
         .scan_prefix(user_id.to_be_bytes())
@@ -139,16 +113,11 @@ pub async fn list(
             let (k, v) = res?;
             let activity_id = u64::from_be_bytes(k[8..16].try_into().unwrap());
             let status: Status = bincode::deserialize(&v)?;
-            let (pending, accepted, activity) = count_status(&db, activity_id)?;
-            Ok((
-                activity_id,
-                ActivityStats {
-                    activity,
-                    status,
-                    accepted,
-                    pending,
-                },
-            ))
+            let activity = activities_tree
+                .get(activity_id.to_be_bytes())?
+                .expect("Missing activity_id");
+            let activity = bincode::deserialize(&activity)?;
+            Ok((activity_id, ActivityStats { activity, status }))
         })
         .collect::<Result<HashMap<_, _>, _>>()?;
     Ok(HttpResponse::Ok().json(activities))
@@ -167,22 +136,59 @@ pub async fn change_status(
 ) -> Result<HttpResponse, Error> {
     let user_id: u64 = session.get(&db)?;
     let activities_user_tree = db.open_tree(ACTIVITIES_USER_TREE)?;
+    let activities_tree = db.open_tree(ACTIVITIES_TREE)?;
     let mut key = Vec::with_capacity(16);
     let status = bincode::serialize(&params.status)?;
     key.extend_from_slice(&user_id.to_be_bytes());
     key.extend_from_slice(&params.activity_id.to_be_bytes());
-    let result = activities_user_tree.transaction(|activities_user_tree| {
-        if activities_user_tree.insert(key.as_slice(), status.as_slice())? == None {
-            sled::transaction::abort(())
-        } else {
-            Ok(())
-        }
-    });
+    let result = (&activities_tree, &activities_user_tree).transaction(
+        |(activities_tree, activities_user_tree)| {
+            if let Some(old_status) =
+                activities_user_tree.insert(key.as_slice(), status.as_slice())?
+            {
+                let old_status: Status = bincode::deserialize(&old_status).map_err(|err| {
+                    sled::transaction::ConflictableTransactionError::Abort(Abort::BincodeError(err))
+                })?;
+                if old_status != params.status {
+                    let activity = activities_tree
+                        .get(params.activity_id.to_be_bytes())?
+                        .expect("Missing activity_id");
+                    let mut activity: Activity =
+                        bincode::deserialize(&activity).map_err(|err| {
+                            sled::transaction::ConflictableTransactionError::Abort(
+                                Abort::BincodeError(err),
+                            )
+                        })?;
+                    match old_status {
+                        Status::Pending => activity.pending -= 1,
+                        Status::Accepted => activity.accepted -= 1,
+                        _ => (),
+                    }
+                    match params.status {
+                        Status::Pending => activity.pending += 1,
+                        Status::Accepted => activity.accepted += 1,
+                        _ => (),
+                    }
+                    let activity = bincode::serialize(&activity).map_err(|err| {
+                        sled::transaction::ConflictableTransactionError::Abort(Abort::BincodeError(
+                            err,
+                        ))
+                    })?;
+                    activities_tree.insert(&params.activity_id.to_be_bytes(), activity)?;
+                }
+                Ok(())
+            } else {
+                sled::transaction::abort(Abort::NotAllowed)
+            }
+        },
+    );
     match result {
         Ok(()) => Ok(HttpResponse::Ok().finish()),
         Err(sled::transaction::TransactionError::Storage(err)) => Err(Error::SledError(err)),
-        Err(sled::transaction::TransactionError::Abort(())) => {
-            Ok(HttpResponse::Forbidden().finish())
-        }
+        Err(sled::transaction::TransactionError::Abort(abort)) => match abort {
+            Abort::NotFound => Ok(HttpResponse::NotFound().finish()),
+            Abort::NotAllowed => Ok(HttpResponse::Forbidden().finish()),
+            Abort::BincodeError(err) => Err(Error::BincodeError(err)),
+        },
     }
 }
